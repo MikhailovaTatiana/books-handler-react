47b2ea00514c65f8841f3dc53ef4628991200181
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-26 14:51:23 +0200

    books handler

---
+++ src/Components/AddBook.tsx
@@ -0,0 +1,75 @@
+import { FormEventHandler, useState } from 'react';
+import './AddBook.css'
+import { BookInterface } from '../utils/BookInterface';
+import Button from './Button';
+
+interface AddBookProps {
+    addBookFunction: (bookToAdd: BookInterface) => void;
+    toggleAddView: () => void;
+}
+
+// Declare component AddBook that receives the props addBookFunction and toggleAddView 
+export default function AddBook({ addBookFunction, toggleAddView }: AddBookProps) {
+    const [genres, setGenres] = useState<string[]>([]);
+    const [genreToAdd, setGenreToAdd] = useState<string>('');
+
+    // Helper function to add a book
+    const addBook: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        const bookData = new FormData(e.target as HTMLFormElement);
+        const newBook = {
+            title: bookData.get('title') as string,
+            author: bookData.get('author') as string,
+            isbn: parseInt(bookData.get('isbn') as string),
+            genres: genres,
+            rating: parseInt(bookData.get('rating') as string),
+            cover: bookData.get("cover") as string
+        }
+        addBookFunction(newBook);
+    };
+
+    // Returns the AddBook component in TSX syntax
+    // Each input has an onChange event handler to update the 
+    // field's value and keep it in the component state. We add our
+    // book by finally clicking the button "Add" which propogates the 
+    // new book to our parent component via the prop addBookFunction
+    // Refactor onChange listeners to simplify 
+    return (
+        <article className='addBook-container'>
+            <h3>ADD NEW BOOK</h3>
+            {/* Using an onSubmit event instead of onClick on add button */}
+            <form className='addBook-form' onSubmit={addBook}>
+                <label className='cover-field'>
+                    Cover:
+                    <input placeholder="Cover" id='cover' name='cover' />
+                </label>
+                <label className='author-field'>
+                    Author:
+                    <input required placeholder="Author" id='author' name='author' />
+                </label>
+                <label className='title-field'>
+                    Title:
+                    <input required placeholder="Title" id='title' name='title' />
+                </label>
+                {genres.length > 0 ? <ul className='genres'>{genres.map((genre, i) => <li key={i}>{genre}</li>)}</ul> : null}
+                <section className='genre-field'>
+                    <label>Genres:
+                        <input placeholder="Genre" onChange={(e) => setGenreToAdd(e.target.value)} id='genre' />
+                    </label>
+                    <Button className="add" handler={() => setGenres([...genres, genreToAdd])} name="Add" />
+                </section>
+                <label className='isbn-field'>
+                    ISBN:
+                    <input required placeholder="00000000" id='isbn' name='isbn' />
+                </label>
+                <label className='rating-field'>Rating:
+                    <input required max="5" min="1" type='number' defaultValue={3} id='rating' name='rating' />
+                </label>
+                <section className='buttons-field'>
+                    <Button className='cancel' handler={toggleAddView} name="Cancel" />
+                    <Button type="submit" className='add' name="Add" />
+                </section>
+            </form>
+        </article>
+    )
+}
---
+++ src/Components/Book.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import './Book.css';
+import { BookInterface } from '../utils/BookInterface';
+import Button from './Button';
+
+interface BookProps {
+    book: BookInterface;
+    updateBook: (updatedBook: BookInterface) => void;
+    deleteBook: (deletedBook: BookInterface) => void;
+}
+
+// Book component, destructures props to extract book and updateBook function and sets their type
+function Book({ book, updateBook, deleteBook }: BookProps) {
+
+    // Declare states for book properties
+    const [edit, setEdit] = useState<boolean>(false);
+    const [title, setTitle] = useState<string>(book.title);
+    const [author, setAuthor] = useState<string>(book.author);
+    const [isbn] = useState<number>(book.isbn);
+    const [rating, setRating] = useState<number>(book.rating);
+
+    // Create a list of stars for the books rating
+    const stars = [];
+    for (let i = 0; i < book.rating; i++) {
+        stars.push(<span key={i}>⭐</span>)
+    }
+
+    // Function to update book with new edits
+    const handleEdit = () => {
+
+        // Set updatedBook object to hold new state
+        const updatedBook: BookInterface = {
+            title: title,
+            author: author,
+            isbn: isbn,
+            genres: book.genres,
+            rating: rating,
+            cover: book.cover
+        }
+
+        // Use the updateBook function provided from props to update the book
+        updateBook(updatedBook);
+        // Disable the edit-mode
+        setEdit(false);
+    };
+
+    // Function to delete book
+    const handleDelete = () => {
+        const deletedBook: BookInterface = {
+            title: title,
+            author: author,
+            isbn: isbn,
+            genres: book.genres,
+            rating: rating,
+            cover: book.cover
+        }
+        deleteBook(deletedBook);
+    }
+
+    return (
+        // If edit mode is enabled, show edit fields, else show normal book html
+        <article className="book-container">{edit ?
+            <>
+                <section className='book-info'>
+                    <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
+                    {/* Update fields for title and author */}
+                    <input defaultValue={title} onChange={(e) => setTitle(e.target.value)} id='newTitle' />
+                    <input defaultValue={author} onChange={(e) => setAuthor(e.target.value)} id='newAuthor' />
+                    <p>{book.isbn}</p>
+                    <p>Genres: {book.genres ? book.genres.join(", ") : "Not defined"}</p>
+                    {/* Update field for rating */}
+                    <input max="5" min="1" type='number' defaultValue={rating} onChange={(e) => setRating(parseInt(e.target.value))} id='newRating' />
+                </section>
+                <Button handler={handleDelete} name="Delete" />
+                <Button handler={handleEdit} name="Confirm" />
+            </>
+            :
+            /* Show default book html, not edit mode */
+            <>
+                <section className='book-info'>
+                    <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
+                    <h2>{book.title}</h2>
+                    <p>{book.author}</p>
+                    <p>{book.isbn}</p>
+                    <p>Genres: {book.genres ? book.genres.join(", ") : "Not defined"}</p>
+                    <p>{stars}</p>
+                </section>
+                <Button handler={() => { setEdit(true) }} name="Edit" />
+            </>
+        }
+        </article>
+
+    )
+};
+export default Book;
